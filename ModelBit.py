ffa1f206f958b88da010cb45ee8831fdc1e5e320
StudentCrow <david.marano@estudiantat.upc.edu>
2023-04-27 16:34:02 +0200

    Update ModelBit.py
    Added first approach of init method to the class

--- ModelBit.py
+++ ModelBit.py
@@ -2,4 +2,12 @@ import pygame
 import ModelOrder
 
 class ModelBit:
-    pass
+    """
+    Model part class that controls each individual bit
+    """
+    def __init__(self, time = 0.0, working = False, subsystem = False, critic = False):
+        self.time = time    #Determines the remining lifetime of the bit
+        self.working = working  #Boolean that indicates if the bit is currently working or in standby
+        self.subsystem = subsystem  #Boolean that indicates if the bit is working in a subsystem or not
+        self.critic = critic    #Boolean that indicates if the bit is in critic state or not
+        self.loc = 'BIOS'   #Location where the bit is currently positionated
