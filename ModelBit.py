7bc9a070938cc8078e087f8d02371c243a70b4ce
StudentCrow <david.marano@estudiantat.upc.edu>
2023-04-28 11:37:32 +0200

    Update ModelBit.py, ReceiveOrder()
    Prepared all the condition that detects the type of order that it is and what method it activates. Important to revise the DecomposedOrd and numwords, and maybe have to reincorpore pygame to python 3.11

--- ModelBit.py
+++ ModelBit.py
@@ -14,15 +14,6 @@ class ModelBit:
     """
     Model part class that controls each individual bit
     """
-    #List of hints of the orders a bit can get
-    OrdList = ['GO TO', 'MINE', 'MOVE', 'FIX', 'GET', 'STORE']
-    #Complete orders that a bit can get:
-    #    GO TO LOCATION, 3
-    #    MINE, 1
-    #    MOVE RESOURCE FROM LOCATION TO LOCATION, 5
-    #    FIX LOCATION, 2
-    #    GET RESOURCE FROM LOCATION, 4
-    #    STORE RESOURCE TO LOCATION, 4
     def __init__(self, time = 0.0, working = False, subsystem = False, critic = False):
         self.time = time    #Determines the remining lifetime of the bit
         self.working = working  #Boolean that indicates if the bit is currently working or in standby
@@ -30,20 +21,61 @@ class ModelBit:
         self.critic = critic    #Boolean that indicates if the bit is in critic state or not
         self.loc = 'BIOS'   #Location where the bit is currently positionated
 
+        # List of hints of the orders a bit can get
+        self.OrdList = ['MINE', 'FIX', 'GO TO', 'GET', 'STORE', 'MOVE']
+        # Complete orders that a bit can get:
+        #    GO TO LOCATION, 3
+        #    MINE, 1
+        #    MOVE RESOURCE FROM LOCATION TO LOCATION, 5
+        #    FIX LOCATION, 2
+        #    GET RESOURCE FROM LOCATION, 4
+        #    STORE RESOURCE TO LOCATION, 4
+
     #Function that checks if an order is real and calls for the correct method
     def ReceiveOrder(self, Ord):    #The variable Ord will be the order obtained from ModelOrder.CheckOrder
-        DecomposedOrd = Ord.split #We get each word of the order into a new list
+        DecomposedOrd = Ord.split() #We get each word of the order into a new list
         numwords = len(DecomposedOrd)
         match numwords: #Determines what to deppending on the value of numwords
-            case 1:
-                pass
-            case 2:
-                pass
-            case 3:
-                pass
-            case 4:
-                pass
-            case 5:
-                pass
+            case 1: #Case when the order is mine
+                for i in self.OrdList:
+                    if self.OrdList.index(i) == 0:
+                        if i ==  DecomposedOrd:
+                            pass    #Start mine method
+                        else:
+                            raise InvalidOrderError('INVALID ORDER')
+            case 2: #Case when the order is fix
+                for i in self.OrdList:
+                    if self.OrdList.index(i) == 1:
+                        if i == DecomposedOrd[0]:
+                            pass    #Start fix method
+                        else:
+                            raise InvalidOrderError('INVALID ORDER')
+            case 3: #Case when the order is go to
+                for i in self.OrdList:
+                    if self.OrdList.index(i) == 2:
+                        reference = i.split()
+                        if reference[0] == DecomposedOrd[0] and reference[1] == DecomposedOrd[1]:
+                            pass    #Start goto method
+                        else:
+                            raise InvalidOrderError('INVALID ORDER')
+            case 4: #Case when the order is get or store
+                for i in self.OrdList:
+                    if self.OrdList.index(i) == 3:
+                        GetReference = i
+                        StoreReference = self.OrdList[self.OrdList.index(i)+1]
+                        if GetReference == DecomposedOrd[0]:
+                            pass    #Start get method
+                        elif StoreReference == DecomposedOrd[0]:
+                            pass    #Start store method
+                        else:
+                            raise InvalidOrderError('INVALID ORDER')
+            case 5: #Case when the order is move
+                for i in self.OrdList:
+                    if self.OrdList.index(i) == 5:
+                        reference = i
+                        if reference == DecomposedOrd[0]:
+                            pass    #Start move method
+                        else:
+                            raise InvalidOrderError('INVALID ORDER')
             case _: #Raises custom error
-                raise InvalidOrderError('Invalid order')
+                raise InvalidOrderError('INVALID ORDER')
