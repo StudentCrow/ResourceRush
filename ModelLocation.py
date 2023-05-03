e39ff61e2614352b678fda76c54625e793311843
StudentCrow <david.marano@estudiantat.upc.edu>
2023-05-03 16:46:11 +0200

    Created ModelAlert.py

--- ModelLocation.py
+++ ModelLocation.py
@@ -5,6 +5,8 @@ class ModelLocation:
     Class for the model part of the interactive locations of the game
     """
     def __init__(self, name, x, y, functional = True, AlertNum = 0):
+        # self.image = pygame.image.load(img_path)    #Load sprite
+
         self.name = name
         self.functional = functional
         self.AlertNum = AlertNum
