895e40d3c12d7fe7a41aca88dfff362500afeb10
StudentCrow <david.marano@estudiantat.upc.edu>
2023-05-12 15:00:21 +0200

    Update ModelLocation.py
    Added the conditional statement to check which location is the one doing the method and how it has to act

--- ModelLocation.py
+++ ModelLocation.py
@@ -5,6 +5,29 @@ class ModelLocation:
     """
     Class for the model part of the interactive locations of the game
     """
+    # Condicional para comprobar que localicación es y por lo tanto cambiar su comportamiento
+    # if self.name == 'PERI':
+    #     pass
+    # elif self.name == 'VRM':
+    #     pass
+    # elif self.name == 'RAM':
+    #     pass
+    # elif self.name == 'ATX':
+    #     pass
+    # elif self.name == 'CPU':
+    #     pass
+    # elif self.name == 'DISK':
+    #     pass
+    # elif self.name == 'CLK':
+    #     pass
+    # elif self.name == 'BIOS':
+    #     pass
+    # elif self.name == 'CHIPSET':
+    #     pass
+    # elif self.name == 'GPU':
+    #     pass
+    # elif self.name == 'VENT':
+    #     pass
     def __init__(self, name, x, y, functional = True, AlertNum = 0):
         # self.image = pygame.image.load(img_path)    #Load sprite
 
