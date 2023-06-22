3bde6fd151a8261d99933e77eda8c21f82ec88a8
StudentCrow <david.marano@estudiantat.upc.edu>
2023-06-23 00:04:36 +0200

    Updated the tests and viewr_bit

--- Pruebas.py
+++ Pruebas.py
@@ -1,5 +1,6 @@
 import pygame, pygame.surfarray
 from pygame.locals import *
+from Viewer_Bit import ViewerBit
 
 
 
@@ -7,20 +8,24 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((640, 480), 0, 24)
+    pygame.display.set_caption("Resource Rush")
+    screen.fill((255, 255, 255))
 
-    surf = pygame.surfarray.pixels3d(screen)
-    surf[:] = (255, 255, 255)
-    surf[::4, ::4] = (0, 0, 255)
+    # surf = pygame.surfarray.pixels3d(screen)
+    # surf[:] = (255, 255, 255)
+    # surf[::4, ::4] = (0, 0, 255)
+
+    bit_prueba = ViewerBit(screen, 320, 240)
+    bit_prueba.draw_bit()
+
     pygame.display.update()
 
-    counter = 10
+    counter = 30
     timer = 1000
-
     timer_event = pygame.USEREVENT + 1
     pygame.time.set_timer(timer_event, timer)
 
     run = True
-    count = 0
     while run:
         clock.tick(60)
         for event in pygame.event.get():
--- Viewer_Bit.py
+++ Viewer_Bit.py
@@ -1,45 +1,15 @@
-# TODO falta hacer que interaccionen con la selección del ratón
-import pygame
-
 from Imports import *
-pygame.init()
 
-# Dimensiones bit
-x_bit_min = 5
-y_bit_min = 5
-x_bit_max = 10
-y_bit_max = 10
+class ViewerBit:
+    def __init__(self, display, x = 0, y = 0, mult = 0, fill_color = (255, 255, 255)):
+        self.display = display
+        self.x = x
+        self.y = y
+        self.mult = mult
+        self.fill_color = fill_color
+        self.border_color = (0, 0, 0)
 
-# Creo ventana y título de ventana
-window = pygame.display.set_mode(WINDOW_DIM)
-pygame.display.set_caption("Resource Rush")
-
-# Coordenadas y dimensiones del bit
-bit_rect = pygame.draw.rect(window, WHITE, (0.5 * WIDTH, 0.5 * WIDTH, x_bit_max, y_bit_max))
-
-
-arrastrando = False
-inicio_seleccion = None
-final_seleccion = None
-
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                arrastrando = True
-                inicio_seleccion = pygame.mouse.get_pos()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                if arrastrando:
-                    arrastrando = False
-                final_seleccion = pygame.mouse.get_pos()
-                print("Posición inicial:", inicio_seleccion)
-                print("Posición final:", final_seleccion)
-
-
-    pygame.display.flip()
-
-pygame.quit()
+    #Function to draw the full bit
+    def draw_bit(self):
+        bit_fill = pygame.draw.rect(self.display, self.fill_color, (self.x, self.y, 50, 50), 0)
+        bit_border = pygame.draw.rect(self.display, self.border_color, (self.x, self.y, 50, 50), 2)
