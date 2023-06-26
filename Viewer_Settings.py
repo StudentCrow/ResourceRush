6e9e2d588f623c592f520e2f04b510c0f41e89b7
PersonalSpace-Invader <131524189+PersonalSpace-Invader@users.noreply.github.com>
2023-06-26 16:03:53 +0200

    26-06 (3)

--- Viewer_Settings.py
+++ Viewer_Settings.py
@@ -5,16 +5,35 @@ def Settings():
     pygame.display.set_caption("SETTINGS")
     # Fuente y títulos
     font = pygame.font.SysFont("Showcard Gothic", 45)
-    title_text = font.render("SETTINGS", True, GREEN)
-    title_rect_coord = title_text.get_rect(center=(WIDTH / 2, 0.3 * HEIGHT))
+    settings_text = font.render("SETTINGS", True, GREEN)
+    title_fps = font.render("Fps", True, BLUE)
+    title_volume = font.render("Volume", True, BLUE)
+    title_resolution = font.render("Resolution", True, BLUE)
+
+    # coordenadas de títulos
+    settings_text_rect_coord = settings_text.get_rect(center=(WIDTH / 2, 0.3 * HEIGHT))
+    title_fps_rect_coord = title_fps.get_rect(center=(WIDTH / 5, 0.5 * HEIGHT))
+    title_volume_rect_coord = title_volume.get_rect(center=(4 * WIDTH / 5, 0.5 * HEIGHT))
+    title_resolution_rect_coord = title_resolution.get_rect(center=(WIDTH / 2, 0.3 * HEIGHT))
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         window.fill(BLACK)
-        pygame.draw.rect(window, BLACK, title_rect_coord)
-        window.blit(title_text, title_rect_coord)
+
+        pygame.draw.rect(window, BLACK, settings_text_rect_coord)
+        pygame.draw.rect(window, BLACK, title_fps_rect_coord)
+        pygame.draw.rect(window, BLACK, title_volume_rect_coord)
+        pygame.draw.rect(window, BLACK, title_resolution_rect_coord)
+
+        window.blit(settings_text, settings_text_rect_coord)
+        window.blit(title_fps, title_fps_rect_coord)
+        window.blit(title_volume, title_volume_rect_coord)
+        window.blit(title_resolution, title_resolution_rect_coord)
+
         pygame.display.update()
 
+Settings()
 pygame.quit()
