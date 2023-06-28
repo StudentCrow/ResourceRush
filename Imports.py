cbc1bd11a034c00b73204fd26370ed0cd24d286d
StudentCrow <david.marano@estudiantat.upc.edu>
2023-06-28 15:47:02 +0200

    Create Imports.py

---
+++ Imports.py
@@ -0,0 +1,22 @@
+# Imports
+import pygame, pygame_gui, sys, os
+pygame.init()
+
+# COLORES
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
+BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
+GREEN = (0, 255, 0)
+ORANGE = (255, 165, 0)
+RED = (255, 0, 0)
+
+# DIMENSIONES PANTALLA
+WIDTH = 1000
+HEIGHT = 755
+WINDOW_DIM = (WIDTH, HEIGHT)
+
+#RESTRICTIONS
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+numb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "x"]
